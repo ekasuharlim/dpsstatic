f114ab083d9e6791f1c044ac64362083691d3aa2
ekasuharlim <eka.suharlim@gmail.com>
2016-11-10 17:49:46 +0800

    learn more link

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ require 'common/header.php';
         <div class="content">
           <h1>SFC Asia</h1>
           <p class="margin-bottom">Some Description</p>
-          <p><a class="btn btn-white" href="/about">Learn more</a></p>
+          <p><a class="btn btn-white" href="about.php">Learn more</a></p>
         </div>
       </div>
     </div>
